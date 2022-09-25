0990fabcf11e3fd98c91fcda0d2859ef11a36bdb
VirtualGenesis <109311914+VirtualGenesis@users.noreply.github.com>
2022-09-25 15:20:17 +0300

    Rename

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/OMISTAJA/CLionProjects/Ohjelmointi 1
+# Install script for directory: C:/Users/OMISTAJA/CLionProjects/Monikerta
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/OMISTAJA/CLionProjects/Ohjelmointi 1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/OMISTAJA/CLionProjects/Monikerta/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- monikerta.c
+++ monikerta.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int kok1= 0;
+    int kok2 = 0;
+
+    printf("Anna kokonaisluku 1 > ");
+    scanf("%d", &kok1);
+
+    printf("Anna kokonaisluku 2 > ");
+    scanf("%d", &kok2);
+
+    if( kok1 == 0 || kok2 == 0)
+        printf("Luku %d on luvun %d monikerta.", kok1, kok2);
+
+    else if(kok1 % kok2 == 0 )
+        printf("Luku %d on luvun %d monikerta.", kok1, kok2);
+
+    else
+        printf("Luku %d ei ole luvun %d monikerta.", kok1, kok2);
+
+
     return 0;
 }
